f4107c85b272e2c0025d852545f4730f835bd392
WansonChoi <wschoi.bhlab@gmail.com>
2022-09-14 08:45:36 +0900

    (Phasing) succeeded in implementing phasing wrapper.

--- Phasing/__main__.py
+++ Phasing/__main__.py
@@ -1,9 +1,13 @@
 #-*- coding: utf-8 -*-
 
 import os, sys, re
+from os.path import basename, dirname, join, exists
+from shutil import which
 import argparse, textwrap
 
-from Phasing.BEAGLE_Phase import Phasing_wrapper
+from Phasing.BEAGLE_Phase import Phasing_wrapper, VCF2BEAGLE
+# from Phasing.src.BEAGLE import BEAGLE
+from src.PLINK2BEAGLE import PLINK2BEAGLE
 
 std_MAIN_PROCESS_NAME = "\n[Phasing]: "
 std_ERROR_MAIN_PROCESS_NAME = "\n[Phasing::ERROR]: "
@@ -11,8 +15,57 @@ std_WARNING_MAIN_PROCESS_NAME = "\n[Phasing::WARNING]: "
 
 class HATK_Phasing(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, _out, _bfile=None, _vcf=None, _bgl=None, _markers=None,
+                 _linkage2beagle="dependency/linkage2beagle.jar", _beagle2vcf="dependency/beagle2vcf.jar",
+                 _vcf2beagle="dependency/vcf2beagle.jar", _beagle=which("beagle"), _plink=which("plink"),
+                 _java=which("java"), _java_mem='1G', _nthreads=1, _f_save_intermediates=False, _f_gzip=True):
+
+        """
+        Input must be in beagle file format anyway.
+            1. PLINK -> Beagle
+            2. VCF -> Beagle
+            3. Beagle
+
+        """
+
+        self.out_prefix = _out
+        self.out_dir = dir(_out)
+
+        self.bgl = None
+        self.markers = None
+        self.bgl_phased = None # main result.
+
+        self.input_file_format = getInputFileFormat(_bfile, _vcf, _bgl, _markers)
+
+        ### Setting BEAGLE input.
+        if self.input_file_format == "PLINK":
+            self.bgl, self.markers = PLINK2BEAGLE(_bfile, self.out_prefix +".P2B",
+                                                  _linkage2beagle, _plink, _java, _java_mem, _f_save_intermediates)
+        elif self.input_file_format== "VCF":
+            # use 'VCF2BEAGLE'
+            pass
+        else:
+            self.bgl = _bgl
+            self.markers = _markers
+
+        ### Phasing (Main)
+        self.bgl_phased = \
+            Phasing_wrapper(self.bgl, self.markers, self.out_prefix, _beagle2vcf, _vcf2beagle, _beagle,
+                            _java, _java_mem, _nthreads, _f_save_intermediates, _f_gzip)
+
+
+        print(self.markers)
+        print(self.bgl_phased)
+
+
+def getInputFileFormat(_bfile, _vcf, _beagle, _markers):
+
+    if bool(_bfile):
+        return "PLINK"
+    elif bool(_vcf):
+        return "VCF"
+    else:
+        return "BEAGLE"
 
 
 
@@ -39,11 +92,20 @@ if __name__ == "__main__":
     parser.add_argument("--bgl", help="\nBeagle(v3.x.x) file.\n\n")
     parser.add_argument("--markers", help="\nThe '*.markers' file along with Beagle(v3.x.x) file.\n\n")
 
-    parser.add_argument("--vcf", help="\nThe VCF file.\n\n")
+    # parser.add_argument("--vcf", help="\nThe VCF file.\n\n")
 
     parser.add_argument("--out", help="\nOutput file prefix\n\n", required=True)
 
     parser.add_argument("--save-intermediates", help="\nDon't remove intermediate files.\n\n", action='store_true')
+
+    # java
+    parser.add_argument("--java-mem", "-mem",
+                        help="\nHeap memory size for each BEAGLE implementation (default: 2g).\n\n", default="2g")
+
+    parser.add_argument("--nthreads", "-nth",
+                        help="\nThe number of threads for each BEAGLE implementation (default: 1).\n\n", default=1,
+                        type=int)
+
 
     ##### <for Test> #####
 
@@ -56,3 +118,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     print(args)
+
+    HATK_Phasing(args.out, args.bfile, None, args.bgl, args.markers,
+                 _f_save_intermediates=args.save_intermediates, _java_mem=args.java_mem, _nthreads=args.nthreads)
