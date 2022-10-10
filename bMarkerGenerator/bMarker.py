b9a5674436421e3cb79521fd7226adbc864593e1
WansonChoi <wschoi.bhlab@gmail.com>
2022-10-10 12:22:21 +0900

    (bMarker.py) now checks target HLA genes in BIM file. (This is needed for Heatmap plot in 'HLA_Study.py'.)

--- bMarkerGenerator/bMarker.py
+++ bMarkerGenerator/bMarker.py
@@ -7,72 +7,34 @@ import pandas as pd
 from src.HATK_Error import HATK_InputPreparation_Error, RaiseError
 from src.PLINK_GT import GT
 from src.util import Exists
+from bMarkerGenerator.checkBIM import checkBIM
+
 
 class bMarker(GT):
 
     def __init__(self, _file_prefix):
 
+        ### Init. of super class ###
+        super().__init__(_file_prefix)
+
+
         ### Main Variables ###
         self.FRQ = _file_prefix+'.FRQ.frq' if Exists(_file_prefix+'.FRQ.frq') else None
 
-        # SNP marker
+        # Marker count.
+        self.m_total = -1
         self.m_SNP = -1
 
-        # HLA marker
         self.m_HLA = -1
         self.m_AA = -1
         self.m_SNPS = -1
         self.m_INS = -1
 
+        self.l_HLA_target = []
+
 
         ### Main Actions ###
-        super().__init__(_file_prefix)
         self.checkBIM()
-
-
-    def checkBIM(self):
-
-        df_bim = pd.read_csv(self.bim, sep='\s+', header=None, dtype=str)
-        # print("df_bim:\n{}\n".format(df_bim))
-
-        ## HLA markers
-
-        # HLA
-        p_HLA = re.compile(r'^HLA_')
-        f_HLA = np.array([bool(p_HLA.match(label)) for label in df_bim.iloc[:, 1].values])
-        df_bim_HLA = df_bim.loc[f_HLA, :]
-        # print("df_bim_HLA:\n{}\n".format(df_bim_HLA))
-
-        # AA
-        p_AA = re.compile(r'^AA_')
-        f_AA = np.array([bool(p_AA.match(label)) for label in df_bim.iloc[:, 1].values])
-        df_bim_AA = df_bim.loc[f_AA, :]
-        # print("df_bim_AA:\n{}\n".format(df_bim_AA))
-
-        # SNPS
-        p_SNPS = re.compile(r'^SNPS_')
-        f_SNPS = np.array([bool(p_SNPS.match(label)) for label in df_bim.iloc[:, 1].values])
-        df_bim_SNPS = df_bim.loc[f_SNPS, :]
-        # print("df_bim_SNPS:\n{}\n".format(df_bim_SNPS))
-
-        # INS
-        p_INS = re.compile(r'^INS_')
-        f_INS = np.array([bool(p_INS.match(label)) for label in df_bim.iloc[:, 1].values])
-        df_bim_INS = df_bim.loc[f_INS, :]
-        # print("df_bim_INS:\n{}\n".format(df_bim_INS))
-
-        # SNP
-        f_SNP = ~(f_HLA | f_AA | f_SNPS | f_INS)
-        df_bim_SNP = df_bim.loc[f_SNP, :]
-        # print("df_bim_SNP:\n{}\n".format(df_bim_SNP))
-
-
-        self.M_markers = df_bim.shape[0]
-        self.m_SNP = df_bim_SNP.shape[0]
-        self.m_HLA = df_bim_HLA.shape[0]
-        self.m_AA = df_bim_AA.shape[0]
-        self.m_SNPS = df_bim_SNPS.shape[0]
-        self.m_INS = df_bim_INS.shape[0]
 
 
     def __repr__(self):
@@ -89,7 +51,7 @@ class bMarker(GT):
         str_N_samples = \
             "- # of samples: {}\n".format(self.N_samples)
         str_M_markers = \
-            "- # of Total markers: {}\n".format(self.M_markers)
+            "- # of Total markers: {}\n".format(self.m_total)
 
         str_m_SNP = \
             "   - # of SNP markers: {}\n".format(self.m_SNP)
@@ -107,14 +69,24 @@ class bMarker(GT):
         str_hasPheInfo = \
             "- has Phenotype Info?: {}\n".format(self.f_hasPheInfo)
 
+        str_HLA_target = \
+            "- HLA genes: {}\n".format(self.l_HLA_target)
+
 
         str_summary = ''.join([
             str_BED, str_BIM, str_FAM, str_FRQ,
             str_N_samples, str_M_markers, str_m_SNP, str_m_HLA, str_m_AA, str_m_SNPS, str_m_INS,
-            str_hasSexInfo, str_hasPheInfo
+            str_hasSexInfo, str_hasPheInfo, str_HLA_target
         ]).rstrip('\n')
 
         return str_summary
+
+
+    def asOldMarkerLabel(self): return 0 # Later. Modify marker labels in bim file (ex. HLA_A*01:01 => HLA_A_0101)
+    def asNewMarkerLabel(self): return 0 # Later. Modify marker labels in bim file (ex. HLA_A_0101 => HLA_A*01:01)
+    def checkBIM(self):
+        self.m_total, self.m_SNP, self.m_HLA, self.m_AA, self.m_SNPS, self.m_INS, self.l_HLA_target = \
+        checkBIM(self.bim)
 
 
 
@@ -130,3 +102,50 @@ if __name__ == '__main__':
     r = bMarker(bmarker)
     print(r)
     pass
+
+
+
+
+    # def checkBIM(self):
+    #
+    #     df_bim = pd.read_csv(self.bim, sep='\s+', header=None, dtype=str)
+    #     # print("df_bim:\n{}\n".format(df_bim))
+    #
+    #     ## HLA markers
+    #
+    #     # HLA
+    #     p_HLA = re.compile(r'^HLA_')
+    #     f_HLA = np.array([bool(p_HLA.match(label)) for label in df_bim.iloc[:, 1].values])
+    #     df_bim_HLA = df_bim.loc[f_HLA, :]
+    #     # print("df_bim_HLA:\n{}\n".format(df_bim_HLA))
+    #
+    #     # AA
+    #     p_AA = re.compile(r'^AA_')
+    #     f_AA = np.array([bool(p_AA.match(label)) for label in df_bim.iloc[:, 1].values])
+    #     df_bim_AA = df_bim.loc[f_AA, :]
+    #     # print("df_bim_AA:\n{}\n".format(df_bim_AA))
+    #
+    #     # SNPS
+    #     p_SNPS = re.compile(r'^SNPS_')
+    #     f_SNPS = np.array([bool(p_SNPS.match(label)) for label in df_bim.iloc[:, 1].values])
+    #     df_bim_SNPS = df_bim.loc[f_SNPS, :]
+    #     # print("df_bim_SNPS:\n{}\n".format(df_bim_SNPS))
+    #
+    #     # INS
+    #     p_INS = re.compile(r'^INS_')
+    #     f_INS = np.array([bool(p_INS.match(label)) for label in df_bim.iloc[:, 1].values])
+    #     df_bim_INS = df_bim.loc[f_INS, :]
+    #     # print("df_bim_INS:\n{}\n".format(df_bim_INS))
+    #
+    #     # SNP
+    #     f_SNP = ~(f_HLA | f_AA | f_SNPS | f_INS)
+    #     df_bim_SNP = df_bim.loc[f_SNP, :]
+    #     # print("df_bim_SNP:\n{}\n".format(df_bim_SNP))
+    #
+    #
+    #     self.m_total = df_bim.shape[0]
+    #     self.m_SNP = df_bim_SNP.shape[0]
+    #     self.m_HLA = df_bim_HLA.shape[0]
+    #     self.m_AA = df_bim_AA.shape[0]
+    #     self.m_SNPS = df_bim_SNPS.shape[0]
+    #     self.m_INS = df_bim_INS.shape[0]
